e405e6eb87c7341d3d2abeead578f2ce1156b43e
AnyaVrn <anya.voronina95@gmail.com>
2023-10-15 05:34:59 +0500

    First commit

---
+++ src/test/java/org/example/PostmanEchoTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class PostmanEchoTest {
+    @Test
+    public void shouldPostRawTextTest() {
+
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+}
