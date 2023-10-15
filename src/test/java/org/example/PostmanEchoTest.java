fe244023d40066f356ae33ff5ad380a0fe150b98
AnyaVrn <anya.voronina95@gmail.com>
2023-10-15 05:56:57 +0500

    4 commit

--- src/test/java/org/example/PostmanEchoTest.java
+++ src/test/java/org/example/PostmanEchoTest.java
@@ -4,7 +4,7 @@ package org.example;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTest {
     @Test
